9a590160d6b959d5a648051eefb3e83ef0fa1b3f
volodymyrkovalcuk <vokovalchuk1995@gmail.com>
2024-07-07 17:17:50 +0300

    change port

--- src/main/java/org/example/azcosmos/controller/UserController.java
+++ src/main/java/org/example/azcosmos/controller/UserController.java
@@ -1,8 +1,11 @@
 package org.example.azcosmos.controller;
 
 import com.azure.core.annotation.Get;
+import org.apache.commons.logging.Log;
 import org.example.azcosmos.entity.User;
 import org.example.azcosmos.repo.UserRepo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +21,7 @@ import java.util.stream.StreamSupport;
 public class UserController {
 
     private final UserRepo userRepo;
+    private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
 
     public UserController(UserRepo userRepo) {
         this.userRepo = userRepo;
@@ -27,6 +31,7 @@ public class UserController {
     public ResponseEntity<List<User>> getAll() {
         List<User> result = StreamSupport.stream(userRepo.findAll().spliterator(), false)
                 .toList();
+        LOG.info(result.get(0).getRole());
         return ResponseEntity.ok(result);
 //        return ResponseEntity.ok(List.of("hello"));
     }
--- src/main/java/org/example/azcosmos/entity/User.java
+++ src/main/java/org/example/azcosmos/entity/User.java
@@ -16,4 +16,28 @@ public class User {
     private String role;
 
     private String name;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
